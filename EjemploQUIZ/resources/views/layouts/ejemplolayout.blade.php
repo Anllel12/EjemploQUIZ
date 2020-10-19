665e44103ed8e242d5abbd269001c4df0471c055
Angel Esquinas <angelesquinaspuig@gmail.com>
2020-10-19 19:02:29 +0200

    HE añadido dos paginas con un boton y cosas

---
+++ EjemploQUIZ/resources/views/layouts/ejemplolayout.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Nombre de la app - @yield('titulo')</title>
+        	    <!-- Scripts -->
+				<script src="{{ asset('js/app.js') }}" defer></script>
+				<!-- Styles -->
+				<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @section('barralateral')
+            Esto es la barra lateral inicial
+        @show
+
+        <div class="container">
+            @yield('contenido')
+        </div>
+    </body>
+</html>
